0781d6328d68b41c44b48e279bd34a0a98ad4897
zhangxiukai <m15237353395@163.com>
2019-11-28 20:31:17 +0800

    add redirect first page

--- springboot/src/main/java/com/demo/demo/controller/IndexController.java
+++ springboot/src/main/java/com/demo/demo/controller/IndexController.java
@@ -1,8 +1,13 @@
 package com.demo.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    @RequestMapping("/")
+    public String index() {
+        return "/index.html";
+    }
 }
