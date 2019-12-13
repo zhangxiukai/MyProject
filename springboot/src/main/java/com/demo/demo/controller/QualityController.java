543ee262aa10918745dcb25d23d0ed9d70266c22
zhangxiukai <m15237353395@163.com>
2019-12-13 11:26:50 +0800

    add scheduler test

--- springboot/src/main/java/com/demo/demo/controller/QualityController.java
+++ springboot/src/main/java/com/demo/demo/controller/QualityController.java
@@ -3,11 +3,11 @@ package com.demo.demo.controller;
 import com.demo.demo.service.QualityService;
 import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/scheduler")
 public class QualityController {
 
