be2356346e8a27e37b8f9b5448537e1931b0e32a
zhangxiukai <m15237353395@163.com>
2020-01-19 16:59:49 +0800

    add atomix doc and design mode

---
+++ demo/src/main/java/com/demo/designMode/builder/Client.java
@@ -0,0 +1,83 @@
+package com.demo.designMode.builder;
+
+
+public class Client {
+    public static void main(String[] args) {
+        Builder builder = new ConcreteBuilder();
+        Director director = new Director(builder);
+        Product product = director.construct();
+        product.show();
+    }
+}
+
+class Product {
+
+    private String partA;
+    private String partB;
+    private String partC;
+
+    public void setPartA(String partA) {
+        this.partA = partA;
+    }
+
+    public void setPartB(String partB) {
+        this.partB = partB;
+    }
+
+    public void setPartC(String partC) {
+        this.partC = partC;
+    }
+
+    public void show() {
+        //显示产品的特性
+    }
+}
+
+abstract class Builder {
+
+    //创建产品对象
+    protected Product product = new Product();
+
+    public abstract void buildPartA();
+
+    public abstract void buildPartB();
+
+    public abstract void buildPartC();
+
+    //返回产品对象
+    public Product getResult() {
+        return product;
+    }
+}
+
+class ConcreteBuilder extends Builder {
+
+    public void buildPartA() {
+        product.setPartA("建造 PartA");
+    }
+
+    public void buildPartB() {
+        product.setPartB("建造 PartB");
+    }
+
+    public void buildPartC() {
+        product.setPartC("建造 PartC");
+    }
+}
+
+class Director {
+
+    private Builder builder;
+
+    public Director(Builder builder) {
+        this.builder = builder;
+    }
+
+    //产品构建与组装方法
+    public Product construct() {
+        builder.buildPartA();
+        builder.buildPartB();
+        builder.buildPartC();
+        return builder.getResult();
+    }
+}
